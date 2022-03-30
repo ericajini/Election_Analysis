579288fb83cb32d234a8eef3504e95aff866da3b
Eric Ajini <ericajini@gmail.com>
2022-03-29 20:15:43 -0400

    class demo added

---
+++ python_classwork.py
@@ -0,0 +1,16 @@
+title = "Frankfurter"
+
+years = 80
+
+expert_status = True
+
+print("Nick is a professional " + title)
+
+
+print("He has been coding for " + str(years) + " years")
+
+# Converts a boolean into a string and prints 
+print ("Expert status: " + str(expert_status))
+
+# An f-string accepts all data types without conversion
+print(f"Expert status: {expert_status}")
