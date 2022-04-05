969367481d62c185386c1516de1075ee103be3f8
Eric Ajini <ericajini@gmail.com>
2022-04-04 21:02:00 -0400

    saving module 3 progress

---
+++ Date_time.py
@@ -0,0 +1,31 @@
+# # Import the datetime class from the datetime module.
+# import datetime as dt
+# # Use the now() attribute on the datetime class to get the present time.
+# now = dt.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
+
+# # Import the datetime class from the datetime module.
+# import datetime
+# # Use the now() attribute on the datetime class to get the present time.
+# now = datetime.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
+
+
+# import csv
+# dir(csv)
+#. The dir() function will return all the functions available on that variable.
+
+
+# For the following data types and data structures, use the dir() function to find all the attributes and methods.
+# int
+# float
+# bool
+# list
+# tuple
+# dict
+# datetime
+# Here are some other modules that may be useful later. You will have to import these first.
+# random
+# numpy
